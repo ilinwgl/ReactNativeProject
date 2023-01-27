e58882b0e594b18c7e7c47fb6981c3e66243d7f4
Guanlin Wang <ilinwgl@gmail.com>
2023-01-27 09:48:56 +0100

    Project from expos website

---
+++ StickerSmash/components/EmojiSticker.js
@@ -0,0 +1,43 @@
+import { View, Image } from "react-native";
+import React from "react";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring
+} from "react-native-reanimated";
+import { TapGestureHandler } from 'react-native-gesture-handler';
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+
+const EmojiSticker = ({ imageSize, stickerSource }) => {
+  const scaleImage = useSharedValue(imageSize);
+  const onDoubleTap = useAnimatedGestureHandler({
+    onActive: () => {
+      if (scaleImage.value){
+        scaleImage.value = scaleImage.value * 2;
+      }
+    },
+  });
+  
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
+  return (
+    <View style={{ top: -350 }}>
+      <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+      <AnimatedImage
+        source={stickerSource}
+        resizeMode="contain"
+        style={[imageStyle, { width: imageSize, height: imageSize }]}
+      />
+      </TapGestureHandler>
+    </View>
+  );
+};
+
+export default EmojiSticker;
